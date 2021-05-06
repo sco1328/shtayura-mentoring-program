13b780eacf0e5efa5d9b241b8371b36ecb3163f1
Roman Shtayura <roman_shtayura@epam.com>
2021-05-06 13:37:38 +0300

    Add single-process mode

--- src/main/java/driver/DriverManager.java
+++ src/main/java/driver/DriverManager.java
@@ -28,6 +28,7 @@ public class DriverManager {
             default:
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--headless");
                 chromeOptions.addArguments("--disable-dev-shm-usage");
                 System.setProperty("webdriver.chrome.driver",
                         "/usr/bin/chromedriver");
